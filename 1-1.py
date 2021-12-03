db83cd2c9c3d63d5153c5df9b543027cae3c7c43
DataDrivenAngel <43357571+DataDrivenAngel@users.noreply.github.com>
2021-12-02 23:27:51 -0500

    Create 1-1.py
    draft algo

---
+++ 1-1.py
@@ -0,0 +1,16 @@
+# day 1 part 1
+
+filename = "AOC11.txt"
+
+# open
+
+
+# count number of higher measurements
+count = 0
+prev = measurements[0]
+for measurem in measurements[1:]:
+   if measure > prev
+      count +=1 
+   prev = measuremnt
+
+print(count)
