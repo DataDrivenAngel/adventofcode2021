6de452e3ef3cf0d63a9a499b28bf9f5b6b97568f
angel <buildingwill@gmail.com>
2021-12-05 11:33:00 -0500

    1-2 complete

---
+++ 1-2.py
@@ -0,0 +1,30 @@
+# day 1 part 2
+
+# open
+text = open("1-1-input.txt",'r')
+inpt = text.read().split()
+measurements = [int(line) for line in inpt]
+
+# count number of higher measurements
+count = 0
+window_front = 3
+window_back = 0
+prev = measurements[window_back:window_front]
+
+for measure in (measurements[1:]):
+    window_front +=1
+    window_back +=1
+    window = measurements[window_back:window_front]
+    window_sum = sum(window)
+    prev_sum = sum(prev)
+    if window_sum > prev_sum:
+        count +=1
+        change = " > "
+    else:
+        change = " < "
+    prev = window
+    
+
+print(count)
+# returns 1497
+# which is correct
